2b67ac03bf79210efe380241681ffbdeb1f421b1
Linh Tran <linh@Linhs-MacBook-Pro.local>
2026-06-22 21:18:43 +0900

    Delete middleware.ts

--- middleware.ts
+++
@@ -1,7 +0,0 @@
-import { type NextRequest, NextResponse } from "next/server"
-
-export function middleware(request: NextRequest) {
-  const response = NextResponse.next()
-  response.headers.set("x-pathname", request.nextUrl.pathname)
-  return response
-}
